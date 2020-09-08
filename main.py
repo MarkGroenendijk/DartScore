e53c234247f13b8a0606da442c3bbb010a889e1f
Marin <marinvogel9@gmail.com>
2020-09-08 10:44:33 +0200

    Added multiple players

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import numpy as np
 
-score = 501
+# Get player count
+print("How many players are up for a game?")
+player_count = int(input())
+while player_count <= 0:
+    print("Invalid player count please retry.")
+    player_count = int(input())
 
-while score != 0:
-    points = int(input())
+# Add initial scores to array
+score = []
+for i in range(player_count):
+    score.append(501)
 
-    if points <= score:
-        score -= points
-    print(score)
-print("you've won")
+breaker = True
+while breaker:
+    for i, elem in enumerate(score):
+        print(f"How many points did player {i} score?")
+        points = int(input())
+
+        if points == elem:
+            print(f"Player {i} won")
+            breaker = False
+            break
+        if points < elem:
+            score[i] -= points
+            print(f"player {i} now has {score[i]} points")
+        else:
+            print(f"player {i} overthrew, {score[i]} points remaining")
+
+
+
+
